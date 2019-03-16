537e81465cd58d4db0086959d42f5af3b7c7fd00
Brian Zheng <brian.zheng@titansoft.com.sg>
2019-03-16 09:33:46 +0800

    implement firstOrDefault

--- CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstOrDefaultTests.cs
@@ -1,13 +1,12 @@
-﻿using Lab.Entities;
+﻿using ExpectedObjects;
+using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
-using System;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyFirstOrDefaultTests
     {
         [Test]
@@ -20,9 +19,51 @@ namespace CSharpAdvanceDesignTests
             Assert.IsNull(actual);
         }
 
-        private Employee JoeyFirstOrDefault(IEnumerable<Employee> employees)
+        [Test]
+        public void get_first_element_when_employees_has_elements()
         {
-            throw new NotImplementedException();
+            var employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    FirstName = "Joey",
+                    LastName = "Chen",
+                    Role = Role.Engineer
+                },
+                new Employee()
+                {
+                    FirstName = "Brian",
+                    LastName = "Zheng",
+                    Role = Role.Engineer
+                },
+            };
+
+            var actual = JoeyFirstOrDefault(employees);
+
+            var expected = new Employee()
+            {
+                FirstName = "Joey",
+                LastName = "Chen",
+                Role = Role.Engineer
+            };
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void get_default_int_when_is_empty()
+        {
+            var numbers = new List<int>();
+
+            var actual = JoeyFirstOrDefault<int>(numbers);
+
+            var expected = 0;
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private TSource JoeyFirstOrDefault<TSource>(IEnumerable<TSource> sources)
+        {
+            var enumerator = sources.GetEnumerator();
+            return enumerator.MoveNext() ? enumerator.Current : default(TSource);
         }
     }
 }
